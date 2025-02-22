d149214976fccf52fa057c9efec7d736499aa999
ND005 <nimmaladinesh5@gmail.com>
2025-02-22 20:42:35 +0530

    Updating Order details

---
+++ src/test/java/com/ECommerce/POJO/orderDetailsResponce.java
@@ -0,0 +1,23 @@
+package com.ECommerce.POJO;
+
+public class orderDetailsResponce {
+	private orderDetailsDataResponce data;
+	private String message;
+
+	public orderDetailsDataResponce getData() {
+		return data;
+	}
+
+	public void setData(orderDetailsDataResponce data) {
+		this.data = data;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
--- src/test/java/com/ECommerce/stepdefinition/TC01_All_API_services_of_Ecommerce.java
+++ src/test/java/com/ECommerce/stepdefinition/TC01_All_API_services_of_Ecommerce.java
@@ -1,8 +1,6 @@
 package com.ECommerce.stepdefinition;
 
 import static io.restassured.RestAssured.given;
-
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
