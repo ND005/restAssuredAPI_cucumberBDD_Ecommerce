3a9735ed56b888ff700f7b58610040c6d55bb7f1
ND005 <nimmaladinesh5@gmail.com>
2025-03-14 00:09:48 +0530

    Final'

--- src/test/java/com/ECommerce/TestRunner/TestRunner.java
+++ src/test/java/com/ECommerce/TestRunner/TestRunner.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Test - Features/TC02 - Verify all data services wsing GraphQL.feature", glue = {"com.ECommerce.stepdefinition"})
+@CucumberOptions(features = "src/test/resources/Test - Features", glue = {"com.ECommerce.stepdefinition"})
 public class TestRunner {
 }
